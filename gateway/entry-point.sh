098007dac844e9d3e49b1249087e0c9006ef0699
YAMASHITA, Junji <ysiijj+github@gmail.com>
2025-09-24 02:06:48 +0900

    gatewayにsnat追加

--- gateway/entry-point.sh
+++ gateway/entry-point.sh
@@ -8,6 +8,10 @@ table ip filter {
 		ip saddr 192.168.1.0/24 tcp dport 53 dnat to 172.16.1.1;
 		ip saddr 192.168.1.0/24 udp dport 53 dnat to 172.16.1.1;
 	}
+    chain internal_snat {
+		type nat hook postrouting priority filter;
+		ip saddr 192.168.1.0/24 snat to 172.16.1.250;
+	}
 }
 EOF
 
